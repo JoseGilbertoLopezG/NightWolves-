a62cdae317ba8072708d3dd98f64f2543cdb7e37
Isaac Aceves <isaacn97@debianHSKY>
2020-06-15 12:29:24 -0500

    Repara formato de usuarios en el sitio de Admin

--- HannaEats/users/admin.py
+++ HannaEats/users/admin.py
@@ -9,8 +9,22 @@ class AccountAdmin(UserAdmin):
     add_form = ClienteForm
     form = AccountModifyForm
     model = Account
+    
+    fieldsets = (
+        (None, {'fields': ('correo', 'password', "tipo")}),
+        ('Datos', {'fields': ('nombre',"ap_paterno","ap_materno","telefono")}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2'),
+        }),
+    )
+    
     list_display = ['nombre', 'ap_paterno', 'ap_materno', 'correo', 'telefono']
-    ordering = ['nombre', 'ap_paterno', 'ap_materno', 'correo', 'telefono']
+    ordering = ["tipo", 'nombre', 'ap_paterno', 'ap_materno', 'correo', 'telefono']
 
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Direcciones)
